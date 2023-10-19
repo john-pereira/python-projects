cd3feb703ff523bcae9c24000b76111fa5ab5c1b
john-pereira <johnev90@gmail.com>
2023-10-19 17:22:12 -0300

    Finishing the portfolio project

--- python-web-portfolio/main.py
+++ python-web-portfolio/main.py
@@ -26,14 +26,21 @@ Abaixo voce pode ver alguns projetos feitos em Python. Fique a vontade para me c
 
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[Source Code](https://github.com/john-pereira)")
