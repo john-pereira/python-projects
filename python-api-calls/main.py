8dd3e6ee70bfed59f199523385ad5bbfd41c6b25
john-pereira <johnev90@gmail.com>
2023-11-22 18:39:31 -0300

    Making an api calls from https://newsapi.org/

--- python-api-calls/main.py
+++ python-api-calls/main.py
@@ -1,10 +1,17 @@
 import requests
 
+
 api_key = "7c4b9b99e771442b9bfa64a3b8de7fa9"
+
 url = "https://newsapi.org/v2/everything?q=tesla&from=2023-10-22&sortBy=publishedAt&apiKey=7c4b9b99e771442b9bfa64a3b8de7fa9"
 
+# Make Request
 request = requests.get(url)
+
+# Ge a dictionary with data
 content = request.json()
+
+# Acessing the titles and the discription of the articles
 for article in content["articles"]:
     print(article["title"])
     print(article["description"])
